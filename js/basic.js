a7690de3724ddc4bdb2ef55d943b24f28a40ab59
studentpankaj <pankaj10101993@gmail.com>
2019-06-11 15:57:07 +0530

    file are uploaded

---
+++ js/basic.js
@@ -0,0 +1,8 @@
+/* smooth scrool */
+$('#view-work').on('click',function(){
+    const images=$('#images').position().top;
+
+    $('html ,body').animate({
+        scrollTop:images
+    },900);
+});
